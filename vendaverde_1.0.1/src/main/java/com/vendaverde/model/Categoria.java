bc27566792c3315c8b5eb832614c6787b2cceaa0
Rafyy2102 <rafpromais@yahoo.com.br>
2020-11-11 09:46:48 -0300

    criando repository e controller da tabela categoria

--- vendaverde_1.0.1/src/main/java/com/vendaverde/model/Categoria.java
+++ vendaverde_1.0.1/src/main/java/com/vendaverde/model/Categoria.java
@@ -1,38 +1,33 @@
 package com.vendaverde.model;
 
+import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.NotNull;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.validation.constraints.Size;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
+@Table(name = "tb_categoria")
 public class Categoria {
-	
-	
+		
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	
-	@NotNull
 	@Size(min = 1, max = 150)
 	private String nome;
 	
-	@NotNull
 	private boolean ativo;
 	
-	@NotNull
-	@Size(min = 1, max = 200)
-	private String produtor;
-	
-	public long getId() {return id;}
+	public long getId() {return id;}	
 	public void setId(long id) {this.id = id;}
 	public String getNome() {return nome;}
 	public void setNome(String nome) {this.nome = nome;}
-	public boolean isAtivo() {return ativo;}
+	public boolean isAtivo() {return ativo;}	
 	public void setAtivo(boolean ativo) {this.ativo = ativo;}
-	public String getProdutor() {return produtor;}
-	public void setProdutor(String porduto) {this.produtor = produtor;}	
-	
 }
