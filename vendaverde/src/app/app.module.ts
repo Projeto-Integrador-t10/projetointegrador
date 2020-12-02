636fa6bfab0693762031cff6e4f28484d6b67f06
Rafyy2102 <rafpromais@yahoo.com.br>
2020-12-02 09:52:27 -0300

    vendaverde angular

--- vendaverde/src/app/app.module.ts
+++ vendaverde/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { SobreComponent } from './sobre/sobre.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    SobreComponent
   ],
   imports: [
     BrowserModule,
