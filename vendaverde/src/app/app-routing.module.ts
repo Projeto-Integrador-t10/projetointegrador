3afb1125cb7fe6b854d23c487d9761010da6f9fb
DESKTOP-L3QPPTA\Diana <coelhodiana04@gmail.com>
2020-12-07 09:57:56 -0300

    inclui página de login

--- vendaverde/src/app/app-routing.module.ts
+++ vendaverde/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 import { SobreComponent } from './sobre/sobre.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch:'full'},
   {path: 'home', component: HomeComponent},
-  {path: 'sobre', component: SobreComponent}
+  {path: 'sobre', component: SobreComponent},
+  {path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
