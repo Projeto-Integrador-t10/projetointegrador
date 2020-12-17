8a2b21ecd29ff8c1d3abf28148c43656269b5f30
Diana Coelho <coelhodiana04@gmail.com>
2020-12-17 15:41:27 -0300

    Mostra produtos na home

--- vendaverde/src/app/service/produto.service.ts
+++ vendaverde/src/app/service/produto.service.ts
@@ -17,8 +17,7 @@ export class ProdutoService {
   }
 
   getAllProdutos(): Observable<Produto[]>{
-
-    return this.http.get<Produto[]>('https://oivendaverde.herokuapp.com/produtos', this.token)
+    return this.http.get<Produto[]>('https://oivendaverde.herokuapp.com/produtos')
   }
 
   getByIdProduto(id: number): Observable<Produto>{
